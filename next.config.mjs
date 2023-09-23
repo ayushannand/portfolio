a92a6b19befe44d5fe1796141fcb5285b7fad2bc
Ayush Anand <direct.ayush@gmail.com>
2023-09-24 02:46:24 +0530

    Initial commit

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	experimental: {
+		appDir: true,
+		mdxRs: true,
+	},
+};
+
+export default withContentlayer(nextConfig);
