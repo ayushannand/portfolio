26cd0c68f45ac8021a2ee5d3f9d7c1f48c85d233
Ayush Anand <direct.ayush@gmail.com>
2023-09-28 00:59:02 +0530

    REcommendation dynamic page

---
+++ app/recommendation/[slug]/header.tsx
@@ -0,0 +1,122 @@
+"use client";
+import { ArrowLeft, Eye, Github, Linkedin } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
+
+type Props = {
+	recommendation: {
+		title: string;
+		name?: string;
+		link: string;
+		src: string;
+		role?: string;
+		relation?: string;
+		date?: string;
+		company?: string;
+		description?: string;
+		short_description?: string;
+	};
+};
+export const Header: React.FC<Props> = ({ recommendation }) => {
+	const ref = useRef<HTMLElement>(null);
+	const [isIntersecting, setIntersecting] = useState(true);
+
+
+	useEffect(() => {
+		if (!ref.current) return;
+		const observer = new IntersectionObserver(([entry]) =>
+			setIntersecting(entry.isIntersecting),
+		);
+
+		observer.observe(ref.current);
+		return () => observer.disconnect();
+	}, []);
+
+	return (
+		<header
+			ref={ref}
+			className="relative isolate overflow-hidden bg-gradient-to-tl from-black via-zinc-900 to-black w-[100vw] h-[100vh]"
+		>
+			<div
+				className={`fixed inset-x-0 top-0 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b lg:bg-transparent ${isIntersecting
+						? "bg-zinc-900/0 border-transparent"
+						: "bg-white/10  border-zinc-200 lg:border-transparent"
+					}`}
+			>
+
+				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
+					<div className="flex justify-between gap-8">
+
+						<Link target="_blank" href="https://linkedin.com/in/ayushannand">
+							<Linkedin
+								className={`w-6 h-6 duration-200 hover:font-medium ${isIntersecting
+										? " text-zinc-400 hover:text-zinc-100"
+										: "text-zinc-600 hover:text-zinc-900"
+									} `}
+							/>
+						</Link>
+						<Link target="_blank" href="https://github.com/ayushannand">
+							<Github
+								className={`w-6 h-6 duration-200 hover:font-medium ${isIntersecting
+										? " text-zinc-400 hover:text-zinc-100"
+										: "text-zinc-600 hover:text-zinc-900"
+									} `}
+							/>
+						</Link>
+					</div>
+
+					<Link
+						href="/recommandation"
+						className={`duration-200 hover:font-medium ${isIntersecting
+								? " text-zinc-400 hover:text-zinc-100"
+								: "text-zinc-600 hover:text-zinc-900"
+							} `}
+					>
+						<ArrowLeft className="w-6 h-6 " />
+					</Link>
+				</div>
+			</div>
+
+			//Content
+			<div className="py-10 px-6 sm:px-12 lg:px-24 text-white">
+				<div className="max-w-screen-xl mx-auto">
+					<div className="text-center">
+						<h1 className="text-4xl font-bold tracking-tight sm:text-6xl font-display">
+							{recommendation.title}
+						</h1>
+					</div>
+
+					<div className="mt-10 flex items-center justify-center space-x-4">
+						<div className="w-20 h-20 relative overflow-hidden rounded-full m-4 flex flex-row font-bold text-zinc-100 group-hover:text-white text-[24px] font-display">
+							<Image
+								src={recommendation.src}
+								alt="Profile"
+								layout="fill"
+								objectFit="cover"
+								className="rounded-full"
+							/>
+						</div>
+						<div className="text-left">
+							<p className="font-bold text-[28px] text-gray-300">{recommendation.name}</p>
+							<p className="text-[24px] text-gray-300">{recommendation.role}</p>
+						</div>
+					</div>
+
+					<p className="mt-8 text-lg leading-8 text-gray-300">
+						{recommendation.description}
+					</p>
+
+					<Link href={recommendation.link}>
+						<div className="w-full justify-center flex p-4 text-xl font-bold cursor-pointer">
+							<p className="text-zinc-200 hover:text-zinc-50  flex flex-row items-center">
+								<p>View {recommendation.name}'s LinkedIn Profile</p> <Linkedin className="mx-2" size={20} /> <span aria-hidden="true">&rarr;</span>
+							</p>
+						</div>
+					</Link>
+
+				</div>
+			</div>
+		</header>
+	);
+};
--- app/recommendation/page.tsx
+++ app/recommendation/page.tsx
@@ -1,101 +1,156 @@
-"use client";
 import Link from "next/link";
+import React from "react";
+import { allRecommendations } from "contentlayer/generated";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import MUICard from '@mui/material/Card';
+import { Article } from "./article";
 import Image from "next/image";
-import { CardHeader } from "@mui/material";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Linkedin } from "lucide-react";
 
-const recommendations = [
-	{
-		title: "From Adobe",
-		name: "Dinesh Bhati",
-		description: "",
-		link: "https://www.linkedin.com/in/ayushannand/#recommendations",
-		src: '/dinesh.png',
-		role: 'Engineering Manager IV at Adobe',
-		relation: "Manager",
-		date: 'September 13, 2023',
-		company: 'Adobe'
-	},
-	{
-		title: "From Byju's",
-		name: "Sri Datta Bandreddi",
-		description: "",
-		link: "https://www.linkedin.com/in/ayushannand/#recommendations",
-		src: '/datta.png',
-		role: 'Software Engineer at Byjus',
-		relation: "Mentor",
-		date: 'May 30, 2023',
-		company: "Byju's"
-	},
-];
+export default async function ProjectsPage() {
 
-const titleStyles = {
-	fontSize: '1rem',
-	transitionDuration: '1000ms',
-	color: 'rgb(228 228 231)',
-};
+	const featured = allRecommendations.find((r) => r.slug === "dinesh")!;
+	const featured2 = allRecommendations.find((r) => r.slug === "datta")!;
+	const sorted = allRecommendations
+		.filter(
+			(project) =>
+				project.slug !== featured.slug &&
+				project.slug !== featured2.slug,
+		);
 
-const subheaderStyles = {
-	fontSize: '.8rem',
-	transitionDuration: '1000ms',
-	color: 'rgb(228 228 231)',
-};
-
-const darkTheme = createTheme({
-	palette: {
-		mode: 'dark', // Enable dark mode
-	},
-});
-
-
-export default function RecommendationPage() {
 	return (
-		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+		<div className="relative pb-16">
 			<Navigation />
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-2 lg:gap-16">
-					{recommendations.map((recommendation, index) => (
-						<Card key={index}>
-							<div className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16">
-								<div className="z-10 flex flex-col items-center">
-									<span className="text-xl font-medium duration-150 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-										{recommendation.title}
-									</span>
-								</div>
-								<ThemeProvider theme={darkTheme}>
-									<MUICard className='bg-[#18181b00] border border-[#000000]'>
-										<CardHeader
-											avatar={
-												<Image
-													src={recommendation.src}
-													alt="Profile"
-													width={50} // Set the desired width
-													height={50} // Set the desired height
-													style={{ borderRadius: '50px' }}
-												/>
-											}
-											titleTypographyProps={titleStyles}
-											subheaderTypographyProps={subheaderStyles}
-											title={`${recommendation.name}`}
-											subheader={recommendation.role}
-										/>
-									</MUICard>
-								</ThemeProvider>
-								<Link
-									target="_blank"
-									key={recommendation.name}
-									href={recommendation.link}
-									className="text-[24px] text-zinc-200 inline-block relative transition-transform hover:translate-x-1"
+			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+				<div className="max-w-2xl mx-auto lg:mx-0">
+					<h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
+						Recommendations
+					</h2>
+					<p className="mt-4 text-zinc-400">
+						Recommendations I've earned :)
+					</p>
+				</div>
+				<div className="w-full h-px bg-zinc-800" />
+
+				<div className="grid grid-cols-1 gap-8 mx-auto lg:grid-cols-2 ">
+					<Card>
+						<Link href={`/recommendation/${featured.slug}`}>
+							<article className="relative w-full h-full p-4 md:p-8">
+								<h2
+									id="featured-post"
+									className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
 								>
-									View on LinkedIn{" "}
-									<span aria-hidden="true" className="">&rarr;</span>
-								</Link>
-							</div>
-						</Card>
-					))}
+									{featured.title}
+								</h2>
+								<span className="m-4 flex flex-row font-bold text-zinc-100 group-hover:text-white text-[18px] font-display">
+									<div className="mr-3">
+										<Image src={featured.src}
+											alt="Profile"
+											width={50} // Set the desired width
+											height={50} // Set the desired height
+											style={{ borderRadius: '50px' }}
+										/></div>
+									<div className="flex flex-col">
+									<Link href={featured.link}>
+										<div className="flex flex-row align-bottom items-center">
+											<div className="hover:underline">{featured.name}</div>
+												<Linkedin className="mx-2" size={20} />
+										</div>
+										</Link>
+										<div className="font-[300] text-[14px]">{featured.role}</div>
+									</div>
+									<div>
+
+									</div>
+
+								</span>
+								<p className="my-4 mb-8 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+									{featured.short_description}
+								</p>
+								<div className="absolute bottom-4 md:bottom-8">
+									<p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+										Read more <span aria-hidden="true">&rarr;</span>
+									</p>
+								</div>
+							</article>
+						</Link>
+					</Card>
+
+					<div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
+					<Card>
+						<Link href={`/recommendation/${featured2.slug}`}>
+							<article className="relative w-full h-full p-4 md:p-8">
+								<h2
+									id="featured2-post"
+									className="mt-4 text-3xl font-bold text-zinc-100 group-hover:text-white sm:text-4xl font-display"
+								>
+									{featured2.title}
+								</h2>
+								<span className="m-4 flex flex-row font-bold text-zinc-100 group-hover:text-white text-[18px] font-display">
+									<div className="mr-3">
+										<Image src={featured2.src}
+											alt="Profile"
+											width={50} // Set the desired width
+											height={50} // Set the desired height
+											style={{ borderRadius: '50px' }}
+										/></div>
+									<div className="flex flex-col">
+									<Link href={featured2.link}>
+										<div className="flex flex-row align-bottom items-center">
+											<div className="hover:underline">{featured2.name}</div>
+												<Linkedin className="mx-2" size={20} />
+										</div>
+										</Link>
+										<div className="font-[300] text-[14px]">{featured2.role}</div>
+									</div>
+									<div>
+
+									</div>
+
+								</span>
+								<p className="my-4 mb-8 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+									{featured.short_description}
+								</p>
+								<div className="absolute bottom-4 md:bottom-8">
+									<p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+										Read more <span aria-hidden="true">&rarr;</span>
+									</p>
+								</div>
+							</article>
+						</Link>
+					</Card>
+					</div>
+				</div>
+				<div className="hidden w-full h-px md:block bg-zinc-800" />
+
+				<div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+					<div className="grid grid-cols-1 gap-4">
+						{sorted
+							.filter((_, i) => i % 3 === 0)
+							.map((project) => (
+								<Card key={project.slug}>
+									<Article recommendation={project} />
+								</Card>
+							))}
+					</div>
+					<div className="grid grid-cols-1 gap-4">
+						{sorted
+							.filter((_, i) => i % 3 === 1)
+							.map((project) => (
+								<Card key={project.slug}>
+									<Article recommendation={project} />
+								</Card>
+							))}
+					</div>
+					<div className="grid grid-cols-1 gap-4">
+						{sorted
+							.filter((_, i) => i % 3 === 2)
+							.map((project) => (
+								<Card key={project.slug}>
+									<Article recommendation={project} />
+								</Card>
+							))}
+					</div>
 				</div>
 			</div>
 		</div>
